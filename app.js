f0840d433f5b10980e63242af55c15e698d68e25
Juan-Rodrig <rodriguezvargas.028@gmail.com>
2024-02-05 18:23:35 -0300

    Se completo el formulario V.1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3050
+const port = 3020
 
 app.use(express.static("public"))
 
@@ -9,7 +9,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
 
+app.get("/register.html", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login.html", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
 app.listen(port, ()=>{
     console.log(`Servidor http://localhost:${port} corriendo`)
 })
-
